1d5b4a6751816cb24e1faf3d144085ec2a9e2473
flkt <flkt.crnpio@gmail.com>
2021-06-26 16:07:39 -0500

    links

--- src/components/Pagina1.vue
+++ src/components/Pagina1.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="">
     <h1>“Accesibilidad para la visualización de datos”</h1>
+    <p>
+      Comité organizador WIDS Mexico city 2021
+      PyLadies CDMX - Women in Data Science, Power and Engineering
+      <a href="https://pyladies-cdmx.github.io/wids-cdmx-2021/#inicio"> PyLadies WiDS </a>
+    </p>
+    <p>
+      Magaly Ruiz<br>
+      <a href="https://twitter.com/flktcrnpio">twitter</a> -
+      <a href="https://github.com/flkt-crnpio">github</a> -
+      <a href="https://codepen.io/flkt-crnpio">codepen</a>
+    </p>
   </div>
 </template>
 
